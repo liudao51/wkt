99f4b1f5839d416d47849bf125dccbd98756d47c
liuzw <229883981@qq.com>
2016-07-27 12:35:54 +0800

    增加：Admin模块-控制器基类ModuleBaseController,CommonController

---
+++ Thinks/App/Admin/Controller/CommonController.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * class：CommonController
+ * mark: 具体模块-普通控制器-公共基类, 继承自 <Module_Namespace>\ModuleBaseController
+ *
+ */
+
+namespace Admin\Controller;
+
+class CommonController extends ModuleBaseController
+{
+
+}
---
+++ Thinks/App/Admin/Controller/ModuleBaseController.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * class：ModuleBaseController
+ * mark: 具体模块-公共控制器-公共基类, 继承自<CommonModule_Namespace>\Controller\BaseController
+ *
+ */
+
+namespace Admin\Controller;
+
+use Common\Controller\BaseController;
+
+class ModuleBaseController extends BaseController
+{
+
+}
