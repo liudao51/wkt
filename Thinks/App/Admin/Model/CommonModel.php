91ac788f719cc45dce620f7052613e4ac95e0344
liuzw <229883981@qq.com>
2016-07-27 12:41:42 +0800

    增加：Admin模块-模型基类ModuleBaseModel,CommonModel

---
+++ Thinks/App/Admin/Model/CommonModel.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * class：CommonModel
+ * mark: 具体模块-普通模型-公共基类, 继承自 <Module_Namespace>\ModuleBaseModel
+ *
+ */
+
+namespace Admin\Model;
+
+class CommonModel extends ModuleBaseModel
+{
+
+}
---
+++ Thinks/App/Admin/Model/ModuleBaseModel.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * class：ModuleBaseModel
+ * mark: 具体模块-公共模型-公共基类,, 继承自<CommonModule_Namespace>\Model\BaseModel
+ *
+ */
+
+namespace Admin\Model;
+
+use Common\Model\BaseModel;
+
+class ModuleBaseModel extends BaseModel
+{
+
+}
